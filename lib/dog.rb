05b96bad7c04d0f1bf60dbf008858e33c2950eed
Andres Duarte <andresduarte010@gmail.com>
2016-09-22 20:46:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  def breed
+    @breed
+  end
+end
