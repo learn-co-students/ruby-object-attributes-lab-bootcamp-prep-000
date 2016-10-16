91d57ee71a92347891e93b123da6d91ad284511d
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-16 21:36:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name= (dn)
+    @name=dn
+  end
+  def name()
+    @name
+  end
+  def breed= (dn)
+    @breed=dn
+  end
+  def breed()
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name= (dn)
+    @name=dn
+  end
+  def name()
+    @name
+  end
+  def job= (dn)
+    @job=dn
+  end
+  def job()
+    @job
+  end
+end
