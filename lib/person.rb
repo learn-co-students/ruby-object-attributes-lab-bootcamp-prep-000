ec8f1c79e4e78ef4420c344b1af0d78e01d465d9
Andrew Nease <andrew.nease.code@gmail.com>
2016-11-08 20:58:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(name_given)
+    @name = name_given
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job_given)
+    @job = job_given
+  end
+end
