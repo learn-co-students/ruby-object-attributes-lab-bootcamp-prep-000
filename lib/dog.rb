d3ca1030702f22ac32600f59fa95f1e3701585c9
Robert La Plant <rlaplantjr@sbcglobal.net>
2016-11-22 16:54:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def breed
+    @breed
+  end
+  def breed=(type)
+    @breed = type
+  end
+  def name
+    @name
+  end
+  def name=(dog_name)
+    @name = dog_name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def job
+      @job
+    end
+    def job=(type)
+      @job = type
+    end
+    def name
+      @name
+    end
+    def name=(dog_name)
+      @name = dog_name
+    end
+end
