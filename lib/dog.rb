403f517cbca40b2eba90cdb2ea06e91773878261
gabor santa <gabor.santa@gmail.com>
2017-01-07 21:27:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  #def initialize(name)
+    #@name = name
+  #end
+
+
+  #def name=(new_name)
+    #@name = new_name
+  #end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+
+end
