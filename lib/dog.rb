2a09e864ff215559d5a077fa9d4201ac483d8604
Kimberly Scarabello <kimberly.scarabello@gmail.com>
2017-01-22 01:54:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+def name=(dogs_name)
+  @this_dogs_name=dogs_name
+end
+
+def name
+  @this_dogs_name
+end
+
+def breed=(breed_name)
+  @this_dogs_breed=breed_name
+
+end
+
+def breed()
+  @breed_name
+end
+
+end
