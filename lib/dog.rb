2e801fccf4c1b16c4dbf5180896eaa9811b065bf
Conlin McManus <conlinmcmanus@gmail.com>
2017-01-25 22:59:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_breed)
+    @breed = breed_breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+puts beyonce.name
