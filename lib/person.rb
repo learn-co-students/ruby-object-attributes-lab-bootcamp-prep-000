715b9b4ca05c94f4f42dba1c48bfe98cdc7ea841
James Wilhelm <hello@jamesaverywilhelm.com>
2017-02-17 21:55:06 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(name_title)
+    @name = name_title
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_title)
+    @job = job_title
+  end
+
+  def job
+    @job
+  end
+end
