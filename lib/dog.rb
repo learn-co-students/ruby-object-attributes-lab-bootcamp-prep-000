83a3270cd362579de0f385d5916105b0e0e3e556
Noah Guillory <slokomisu4@gmail.com>
2017-02-23 05:58:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
