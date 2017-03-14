5a4aaf303397e9190048c81e135a4ad34f911ac8
john vascsinec <pensteeler@gmail.com>
2017-03-14 00:45:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dogName)
+    @name = dogName
+  end
+
+  def name
+    @name
+  end
+  
+  def breed=(dogBreed)
+    @breed = dogBreed
+  end
+
+  def breed
+    @breed
+  end
+  
+end
