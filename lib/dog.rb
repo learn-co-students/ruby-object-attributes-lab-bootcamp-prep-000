cb7f3df51398fc63674b74a3a1eb86878ce98aa6
Graham Squires <graham.squires3@gmail.com>
2017-03-20 20:27:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+
+#fido = Dog.new
+#fido.name = "Fido"
+
+#puts fido.name
+
+def breed=(dogs_breed)
+  @breed = dogs_breed
+end
+
+def breed
+  @breed
+end
+end
+#snoopy = Dog.new
+#snoopy.breed = "Beagle"
+#puts snoopy.breed
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+def name=(beyonce)
+  @name = beyonce
+end
+
+def name
+  @name
+end
+
+def job=(singer)
+  @job = singer
+end
+
+def job
+  @job
+end
+end
