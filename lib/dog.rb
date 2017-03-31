395157d353b0dbf2103aee6b77f9ad1990537ae9
Charles Jarju <charlesjarju@gmail.com>
2017-03-31 06:47:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(aname)
+    @name = aname
+  end
+  def name
+    @name
+  end
+  def breed=(abreed)
+    @breed = abreed
+  end 
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(aname)
+    @name = aname
+  end
+  def name
+    @name
+  end
+  def job=(ajob)
+    @job = ajob
+  end
+  def job
+    @job 
+  end
+end
