7d6d1ff594158fa915299600a6c46633bf671152
Brian Lane <mr.brianlane@gmail.com>
2017-05-01 02:36:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name # this is a getter for the name variable
+    @name
+  end
+
+  def name=(new_name) # this is a setter for the name variable
+    @name = new_name
+  end
+
+  def breed # this is a getter for the name variable
+    @breed
+  end
+
+  def breed=(new_breed) # this is a setter for the name variable
+    @breed = new_breed
+  end
+
+end
