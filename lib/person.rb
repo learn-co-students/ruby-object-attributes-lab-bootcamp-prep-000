9759c1fd3ef72fbcf7bba86b23312e3a161d3c24
Jin Cheng <jincheng88@outlook.com>
2017-05-03 16:50:30 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,11 @@
 class Person
-  def initi(name)
-    @name = name
-  end
-
+  
   def name
     @name
   end
 
   def name= (new_name)
     @name = new_name
-  end
-
-  def job(job)
-    @job = job
   end
 
   def job
