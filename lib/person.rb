7782af2238b7ecfe627b733b40e3b65d06b463b9
Mikka Pineda <mikkanthrope@gmail.com>
2017-05-08 02:08:16 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(name_input)
+    @name = name_input
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_input)
+    @job = job_input
+  end
+
+  def job
+    @job
+  end
+
+end
