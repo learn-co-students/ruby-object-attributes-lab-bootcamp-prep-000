a30b8794752b46a262a6be480e85c3ca71b896e5
Tony Nassif <tonynassif@gmail.com>
2017-05-18 23:14:23 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize
+    @name = name
+    @breed = breed
+  end
+  def name 
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end 
+  def breed
+    @breed
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+end
