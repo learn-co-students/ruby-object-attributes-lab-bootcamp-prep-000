5cac2096f8143f56e65ee581fb86672d8759bced
Gregory Fellis <gregfellis@gmail.com>
2017-05-18 00:18:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  # setters
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  # getters
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+end
