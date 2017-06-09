e26cb563065c1292dfc7ecea4daff529a6e5e893
Anton Gegeniger <lex-mark@mail.ru>
2017-06-09 10:12:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(name)
+    @name=name
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed=new_breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name
+    @name
+  end
+  def name=(name)
+    @name=name
+  end
+
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job=new_job
+  end
+end
