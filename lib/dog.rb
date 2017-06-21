5321d75a349a8fbb3a920f8558ae1849bf36a0ab
Karen Yee <td2521@verizon.net>
2017-06-21 04:24:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(value)
+    @name=value
+  end
+  def breed
+    @breed
+  end
+  def breed=(value)
+    @breed=value
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name
+    @name
+  end
+  def name=(value)
+    @name=value
+  end
+  def job
+    @job
+  end
+  def job=(value)
+    @job=value
+  end
+end
