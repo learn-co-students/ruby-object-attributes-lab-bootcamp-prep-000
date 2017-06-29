22bb7c32a0ba9de96147cc62bd43841645317767
chris u <cu364@nyu.edu>
2017-06-29 21:43:38 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+
+class Person
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+
+  def job= (type)
+    @job = type
+  end
+
+  def name= (namePerson)
+    @name = namePerson
+  end
+end
