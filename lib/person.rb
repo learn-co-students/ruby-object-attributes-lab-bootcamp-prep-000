17c4a9a94bf0ef10c59ae31ed7ef9e483f6fab3d
Mohammad Khan <sparrowangels@gmail.com>
2017-08-07 02:28:31 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  def name
+    @name
+  end
+  def job=(person_job)
+    @job=person_job
+  end
+  def job
+    @job
+  end
+end
+
+#
+# You'll be teaching Person about their names through two methods,
+# #name, and #name= that read and write to a corresponding instance variable @name.
+# GIVE A PERSON A JOB
+# You'll be teaching Person about their jobs through two methods,
+#  #job, and #job= that read and write to a corresponding instance variable @job.
