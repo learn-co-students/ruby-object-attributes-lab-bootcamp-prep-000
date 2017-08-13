34dbaaac3bef23b7381bc82078b5c6141d2ee79f
George Liu <georgeliu96@gmail.com>
2017-08-13 23:04:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breedname)
+    @breed = breedname
+  end
+
+  def breed
+    @breed
+  end
+end
