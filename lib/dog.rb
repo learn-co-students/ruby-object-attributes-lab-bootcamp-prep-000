5646d861650ac011337249b7787d15538f030e80
alex s <ansachs@gmail.com>
2017-08-18 17:19:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  def name
+    @name
+  end
+  def name=(full_name)
+    @name = full_name
+  end
+
+  def breed
+    @breed
+  end
+  def breed=(name_breed)
+    @breed = name_breed
+  end
+end
