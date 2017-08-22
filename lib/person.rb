4d0bc641c1a15f8f28663feadf3fa2c84f2b22a2
Hannah Danziger <hdanzig1@jhu.edu>
2017-08-22 12:10:15 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  def name= (name)
+    @name= name
+  end
+
+  def name
+    @name
+  end
+end
+
+class Person
+
+  def job= (job)
+    @job= job
+  end
+
+  def job
+    @job
+  end
+end
