34a7b76a12630a8e86d12a88f7b2f4fe533903a6
gforceroy <gforceroy@hotmail.com>
2017-08-27 03:49:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(name_new)
+    @name = name_new
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(job)
+    @breed = job
+  end
+
+  def breed
+    @breed
+  end
+
+end
