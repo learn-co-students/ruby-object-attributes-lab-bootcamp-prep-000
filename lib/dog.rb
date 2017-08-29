c875908f434a9a6fa3e42a1f06f98ac9d61ffbbb
Chris Nady <nady.christopher@gmail.com>
2017-08-29 00:34:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(full_name)
+    @name = full_name
+  end
+
+  def name
+    @name
+   end
+
+   def breed=(breed_new)
+     @breed = breed_new
+   end
+
+   def breed
+     @breed
+   end
+
+end
