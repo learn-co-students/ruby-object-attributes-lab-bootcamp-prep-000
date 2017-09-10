d4372935679f4436c2ddfde1a979878dbbad707c
W M <wmendoz7@calstatela.edu>
2017-09-10 02:30:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(a_name)
+    @name = a_name
+  end
+
+  def name
+    @name
+  end
+  
+  def breed=(a_breed)
+    @breed = a_breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
