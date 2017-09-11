6a28c4b532ce2b72df1a2eba72dcfa452024288a
John Day <itisjohnday@gmail.com>
2017-09-11 01:16:34 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    name = @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    job = @job
+  end
+
+end
