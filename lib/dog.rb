4c74673e3189c930e1857b03a8dc9a575f5d7e93
Kartik Parihar <kartikvips@gmail.com>
2017-09-14 01:02:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def name=(nombre)
+    @name = nombre
+
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breeed)
+    @breed=breeed
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(nombre)
+    @name = nombre
+  end
+
+  def name
+    @name
+  end
+
+  def job=(dinero)
+    @job=dinero
+  end
+
+  def job
+    @job
+  end
+
+end
