eb9c64591fee38b0e21c3ec9e3c2ea44b91654ff
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-21 21:42:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def intial(intial_name, initial_breed)
+    @name = intial_name
+    @breed = initial_breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+end
