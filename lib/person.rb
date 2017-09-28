2d66dd2e9f06852be65eb3d607166b1a7f7de766
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-09-28 17:01:58 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+#persons_name
+def name
+  @name
+end
+
+def name=(persons_name)
+  @name = persons_name
+end
+
+#persosns_job
+def job
+  @job
+end
+
+def job=(persosns_job)
+  @job = persosns_job
+end
+end
