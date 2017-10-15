e0783607bebd8f6eadeb8bffb0ef6326136c8518
Andrew Siplas <andrew@asiplas.net>
2017-10-15 13:42:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
