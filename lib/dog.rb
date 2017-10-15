d8175b47eb87ec29e5d3ea9d3828cc9c7eb1e5ce
Lawrence Hon <lhon001@gmail.com>
2017-10-15 15:58:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(new_input)
+    @name = new_input
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_input)
+    @breed = new_input
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(new_input)
+    @name = new_input
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_input)
+    @job = new_input
+  end
+end
