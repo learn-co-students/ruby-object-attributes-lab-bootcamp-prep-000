ff2c0df00596f01e75b3b84b3ec11befd2481c29
Shumei Huang <rose1264@gmail.com>
2017-10-25 05:26:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  def initializeName(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def initializeBreed(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
