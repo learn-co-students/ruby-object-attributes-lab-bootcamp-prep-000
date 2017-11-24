e1be10d426e1b9ac311800e5c97d9b74acab0580
Jarrod Dyer <jarrod@gasganno.com>
2017-11-24 05:03:33 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,26 @@
+class Person
+  def name(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+end
