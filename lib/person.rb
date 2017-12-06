c96e15bb90a191442b895253c4cfe4711da0e672
David J Towster <djt4xe@virginia.edu>
2017-12-06 06:35:53 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+
+end
