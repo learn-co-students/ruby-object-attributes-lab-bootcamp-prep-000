a36deb62ecea981074c1cd5b220643a27f7a5548
Alex Bevan <alex@bevan.eu>
2017-12-28 13:45:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  # Class body
+
+  # Instance Method Definition
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
