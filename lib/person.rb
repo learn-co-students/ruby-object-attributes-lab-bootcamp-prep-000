f9f0bc91d739458c7315b4fc192b7cd5090303eb
Richard Loera <richexp9@gmx.com>
2018-01-02 11:31:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def name
+    @name
+  end
+  def job=(dogs_name)
+    @job = dogs_name
+  end
+  def job
+    @job
+  end
+
+end
