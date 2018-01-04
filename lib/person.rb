8c660877dfc803b8955fe966cedf11a46c8a83e7
Sean Clifford <sclifford6262@gmail.com>
2018-01-04 21:59:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(name_change)
+    @name = name_change
+  end
+
+  def name
+     @name
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+  def job
+     @job
+  end
+
+end
