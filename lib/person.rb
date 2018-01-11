a546f3a7390272bf1e560cb0b6dbb4180db10529
Topi Kettunen <topi.kettunen@myy.haaga-helia.fi>
2018-01-11 18:33:42 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(new_name)
+    @job = new_name
+  end
+  
+  def job
+    @job
+  end
+end
+    
