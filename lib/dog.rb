9e18f738ade704f25112cf27d1dfe5661f1b528c
Katrina Brinson <kbrinso@gmail.com>
2018-01-13 02:56:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def name=(dname)
+    @name = dname
+  end 
+  
+  def name
+    @name
+  end
+  
+  def breed=(dbreed)
+    @breed = dbreed
+  end
+  
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name=(p_name)
+    @name = p_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(p_job)
+    @job = p_job
+  end
+  
+  def job
+    @job
+  end
+end
