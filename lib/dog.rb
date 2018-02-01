d22032bc1c8cb0842829ac698ac3ea6b0d4d98da
Michelle B <mjblevins7@gmail.com>
2018-02-01 07:10:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+
+  def breed
+    @breed
+  end
+
+end
