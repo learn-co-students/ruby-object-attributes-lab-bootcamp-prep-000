8842073f7d6086ccfade320535c58c0c9e19ca32
Greg Stimson <stimsong@yahoo.com>
2018-02-07 22:49:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,19 @@
 class Dog
   
-  def initialize(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
-  
+
   def name
     @name
   end
   
-  def name=(new_name)
-    @name = new_name
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    @breed
   end
     
 end
