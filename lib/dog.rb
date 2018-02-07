17ead27d38d56534f2bb56135d7cff1f4fb6fd3f
Greg Stimson <stimsong@yahoo.com>
2018-02-07 22:55:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   
-  def initialize(name)
+  def name= (name)
     @name = name
   end
-
+  
   def name
     @name
   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
   
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def name
