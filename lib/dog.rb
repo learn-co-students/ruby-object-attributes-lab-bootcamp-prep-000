299e327ed823271729dcc205a12c75ef695c67cd
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-13 18:59:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,5 @@
 class Dog 
-  
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
+
   def name
     @name
   end
@@ -21,4 +8,12 @@ class Dog
     @breed
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
 end
