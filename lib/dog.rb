3b70cefed3f0eda835ed7752cfc19e91969413d6
Katherine He <sakura_saki321@yahoo.com>
2018-02-24 23:19:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dog_name) #setter
+    @name = dog_name
+  end
+
+  def name #getter
+    @name
+  end
+
+  def breed=(dog_breed) #setter
+    @breed = dog_breed
+  end
+
+  def breed #getter
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(person_name) #setter
+    @name = person_name
+  end
+
+  def name #getter
+    @name
+  end
+
+  def job=(person_job) #setter
+    @job = person_job
+  end
+
+  def job #getter
+    @job
+  end
+end
