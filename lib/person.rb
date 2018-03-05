aeffe9d1e4dbb9f360e9b275ad4d0b6ba8419f3a
Matt Zuzolo <mattzuzolo@outlook.com>
2018-03-05 21:36:50 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  
+  def name
+    @name
+  end  
+  
+  def name=(name)
+    @name = name
+  end
+  
+  
+  
+  def job
+    @job
+  end  
+  
+  def job=(job)
+    @job = job
+  end
+  
+end
