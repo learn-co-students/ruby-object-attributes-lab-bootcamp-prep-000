f250efaca4f950f953d4eb1e155b006cae4d3702
John Kalinauskas <jkalinauskas@live.com>
2018-04-04 15:23:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class dog
+
+ def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+ end	 
+ 
