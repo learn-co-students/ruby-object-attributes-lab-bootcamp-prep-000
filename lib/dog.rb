698d5190cc09034a58f531bf99dc9817bf2ae21e
Richard Fenix <richard.fenix@gmail.com>
2018-04-10 14:23:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  def breed
+    @breed
+  end
+
+end
