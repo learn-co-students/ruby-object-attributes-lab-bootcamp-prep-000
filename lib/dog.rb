446e8038c32782187960ca9b89268db8a53adf1a
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-27 14:23:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+
+class Person
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  def job=(new_job)
+    @job = new_job
+  end
+end
