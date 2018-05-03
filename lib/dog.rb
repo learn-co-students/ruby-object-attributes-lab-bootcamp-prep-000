5751518b9db093092e52fa35b138c5bf81c5285c
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-05-03 18:19:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def dogs_name(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(fido)
+    @name = fido
+  end
+
+  def dogs_breed(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed=(pug)
+  @breed = pug
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def persons_name(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(something)
+    @name = something
+  end
+  def persons_job(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job=(teacher)
+    @job = teacher
+  end
+end
