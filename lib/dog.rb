d5aecb60f6b7028f585e35755d35dba1dae9d530
Lorena <lorena.giddens@outlook.com>
2018-05-10 18:29:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,40 @@
+class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
+end
+
+class Person
+  
+  def name
+    @name
+  end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job
+  end
+
+end
