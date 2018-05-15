fdfa541429698bb1a93019d88fa08a404fa72188
Jed Peek <jedpeek@yahoo.com>
2018-05-15 17:38:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name= (dog_name) #setter method
+  @name = dog_name
+end
+
+def name #getter method
+  name = @name
+end
+
+def breed= (dog_breed)
+  @breed = dog_breed
+end
+
+def breed
+  breed = @breed
+end
+
+end
