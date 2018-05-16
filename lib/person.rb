3a472bb75d1e69ddda4feba778d1f3f9dc1370ad
jaredianmills <jaredianmills@gmail.com>
2018-05-16 21:11:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(persons_job)
+    @job = persons_job
+  end
+
+  def job
+    @job
+  end
+
+end
