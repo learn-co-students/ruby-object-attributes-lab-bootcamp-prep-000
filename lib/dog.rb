f69801d9e5c7a2d5613438af713d257f685721e3
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-16 16:58:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(dog_name) # setter
+    @name = dog_name
+  end
+
+  def name #getter
+    name = @name
+  end
+
+  def breed=(dog_breed) # setter
+    @breed = dog_breed
+  end
+
+  def breed #getter
+    breed = @breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name) #setter
+    @name = person_name
+  end
+
+  def name #getter
+    name = @name
+  end
+
+  def job=(person_job) #setter
+    @job = person_job
+  end
+
+  def job #getter
+    job = @job
+  end
+end
