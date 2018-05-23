c369b602622c0448296495d90461881626c1777f
Jason Pitts <juniorjr083@gmail.com>
2018-05-23 01:09:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog 
+  
+      
+  
+  
+  def name 
+    @name 
+    
+  end 
+  
+  def name=(name)
+    @name = name
+    
+  end 
+  
+  
+  
+  
+  
+  
+  def breed=(breed)
+   @breed = breed
+
+  
+  
+end
+
+
+
+def breed 
+  @breed 
+  
+end 
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,31 @@
+class Person
+  
+  def name 
+    @name
+  end 
+  
+  
+  def name=(name)
+    
+    @name = name 
+    
+  end 
+  
+  def job=(title)
+    
+    @job = title 
+  end 
+  
+  def job 
+    
+    @job 
+    
+  end 
+  
+    
+  
+  
+  
+  
+  
+end 
