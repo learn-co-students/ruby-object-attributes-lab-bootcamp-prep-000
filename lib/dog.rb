3c49ab1c1b73a4416d5d50e9e0e00ab3fa020627
Sushi Chanrai <sushi@pathworks.co>
2018-06-05 03:03:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+ def name=(new_value) 
+   @name = new_value
+ end
+ 
+ def name
+   @name
+ end 
+ 
+ def breed=(new_value)
+   @breed = new_value
+ end
+ 
+ def breed
+   @breed
+ end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+ def name=(new_value) 
+   @name = new_value
+ end
+ 
+ def name
+   @name
+ end 
+ 
+ def job=(new_value)
+   @job = new_value
+ end
+ 
+ def job
+   @job
+ end
+end
