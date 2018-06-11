8757b40067d78e47ebc2076886a103e18e8be7cb
Shravani PL <pl.shravani@gmail.com>
2018-06-11 14:27:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name 
+    @name 
+  end
+  
+  def name = (name_new)
+    @name = name_new
+  end 
+  
+  def breed 
+    @breed
+  end
+  
+  def breed = (breeds)
+    @breed = breeds 
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name 
+    @name 
+  end
+  
+  def name = (name_new)
+    @name = name_new
+  end 
+  
+  def job 
+    @job
+  end
+  
+  def job = (job_new)
+    @job = job 
+  end
+
+end
