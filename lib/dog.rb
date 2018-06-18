307420eb231ef981e7a6b71cc39452ab51e08829
Robin Börjesson <robin.b@outlook.com>
2018-06-18 18:57:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog
+  def name=(dog_name)
+     @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+class Person
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
+  
+end
