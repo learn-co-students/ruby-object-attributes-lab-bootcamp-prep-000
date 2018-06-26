ae8388639a6dbf09569d9fd4249fec6b4b688994
Forrest Benton <elliottbenton@gmail.com>
2018-06-26 17:24:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name 
+    @name
+  end 
+  
+  def name=(breed_name)
+    @name = breed_name
+  end
+  
+  def breed 
+    @breed
+  end 
+  
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  
+end
