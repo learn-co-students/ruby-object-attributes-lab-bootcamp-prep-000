a6c2f22eab7c3a151c173a70271ac0dceadacb51
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-28 09:27:54 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+  def initialise (name, job)
+    @name = name
+    @job = job
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+end
