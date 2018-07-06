4868e870312d00824e4503d9a3517ad69f8d5a34
T Law <redirect@gmail.com>
2018-07-06 21:37:40 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(name_input)
+    @name = name_input
+  end
+  def name
+    @name
+  end
+  
+  def job=(breed_input)
+    @job = breed_input
+  end
+  def job 
+    @job
+  end
+end
