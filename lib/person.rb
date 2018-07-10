1a9c3d6b54cd764e4b97fa49cc1a502396093e50
josh.e.stroud@gmail.com Stroud <josh.e.stroud@gmail.com>
2018-07-10 03:22:00 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(full_name)
+    @name = full_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_title)
+    @job = job_title
+  end
+  
+  def job 
+    @job 
+  end
+end
