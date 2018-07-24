609c92619f5df3a3ad404ed8ad862f57f47390b3
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-24 21:59:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(cool_name)
+    @name = cool_name
+  end
+  def name 
+    @name
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  def job
+    @job
+  end
+end
