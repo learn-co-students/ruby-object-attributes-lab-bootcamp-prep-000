257b681da74e5349e0f7ce9ea09dff0d5ff4a6f1
Chan Kai Loon <chankailoon@gmail.com>
2018-08-21 05:02:38 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(username)
+    @name = username
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(user_job)
+    @job = user_job
+  end
+  
+  def job
+    @job
+  end
+end
