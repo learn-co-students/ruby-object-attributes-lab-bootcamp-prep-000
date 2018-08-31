b9bd322bc8f1c1acace28354853be92ab27d8d8b
Casey Zimmer <czimmer@ramapo.edu>
2018-08-31 23:21:16 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  
+  def name=(name_in)
+      @name = name_in 
+  end
+  
+  def name
+    @name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job_in)
+    @job = job_in
+  end
+  
+    
+end
