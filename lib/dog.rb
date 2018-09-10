ad71b23b7cd8bf2efd547fd8eb87caf5fa73ecd4
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-09-10 13:39:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(given_name)
+    @name = given_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(given_breed)
+    @breed = given_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
