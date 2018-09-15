a808f1530c691ec748847f8fc693acb271015c23
S H <sashanicolehart@gmail.com>
2018-09-15 16:53:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    name = @name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+  def job
+    job = @job
+  end
+  
+end
