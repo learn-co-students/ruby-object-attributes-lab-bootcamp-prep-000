9a8fa3a8732621cd9de882588f2f82d95abddf64
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-21 17:42:47 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(personname)
+    @name =  personname
+  end
+
+  def name
+    @name
+  end
+
+  def job=(jobb)
+    @job = jobb
+  end
+
+  def job
+    @job
+  end
+
+end
