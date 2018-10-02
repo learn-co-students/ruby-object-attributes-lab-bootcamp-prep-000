4e05af15caeb032800f3e07f31d49bf65ac06cec
Sean Lucius <seanlucius22@icloud.com>
2018-10-02 23:06:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(new_dog_name)
+    @name = new_dog_name
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed 
+    @breed
+  end 
+  
+  def breed=(new_dog_breed)
+    @breed = new_dog_breed
+  end 
+  
+end 
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name 
+    @name
+  end 
+  
+  def name=(new_person_name)
+    @name = new_person_name
+  end 
+  
+  def job
+    @job
+  end 
+  
+  def job=(new_person_job)
+    @job = new_person_job
+  end 
+end 
