72643e551aa36703923d8fbbf8433eecbffd3029
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-06 16:21:24 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  
+  def name 
+    @name 
+  end 
+  
+  def name=(personName)
+    @name = personName
+  end 
+  
+  def job
+    @job 
+  end 
+  
+  def job=(personJob)
+    @job = personJob
+  end 
+  
+end 
