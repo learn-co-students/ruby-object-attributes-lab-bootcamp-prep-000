1ac73ffebeda81c69aaf46bf758b4404c00a4a9a
Iain McKenzie <iainmck29@hotmail.com>
2018-10-08 09:30:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(persons_job)
+    @job = persons_job
+  end
+
+  def job
+    @job
+  end
+end
