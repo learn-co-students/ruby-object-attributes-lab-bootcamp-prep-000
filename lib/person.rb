d5469e084dc356c5b61532579ce44e9f1ceb6535
Nathan Bell <nathanbell09@gmail.com>
2018-10-19 14:05:10 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+  # def initialize(person_name, person_job)
+  #   @name = person_name
+  #   @job = person_job
+  # end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+
+  def job
+    @job
+  end
+
+end
