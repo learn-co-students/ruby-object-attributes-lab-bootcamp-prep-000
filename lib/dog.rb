ccf62c83d24af42d663cdab8d0a7effeb54a0c1e
David Choi <daaave@gmail.com>
2018-10-27 01:52:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  @name
+    
+  def name()
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  @breed
+    
+  def breed()
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+
