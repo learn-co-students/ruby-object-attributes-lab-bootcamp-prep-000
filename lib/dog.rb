245fa359d9de66ab5666f54db2dedc8ad9c381f3
Indu Ramesh <induramesh@gmail.com>
2018-12-04 04:05:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name
+    @name
+  end
+  
+  def name=(newname)
+    @name = newname
+  end 
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(geneticmutant)
+    @breed = geneticmutant
+  end
+  
+end 
