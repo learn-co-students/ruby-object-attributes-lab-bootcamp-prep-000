3e8f1249fa5796cf4f33aae71a808c8386f1ea34
kovner chalumo <kchalumo@gmail.com>
2018-12-17 01:24:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(nom)
+    @name = nom
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(bre)
+    @breed = bre
+  end
+  
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def job
+    @job
+  end
+  
+  def job=(jo)
+    @job = jo
+  end
+  
+  def name=(nom)
+    @name = nom
+  end
+  
+  def name
+    @name
+  end
+end
