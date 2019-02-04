462f8e9532eabe74e63d730ca3c35bb5ac06dd0c
Rufus <ruufman@gmail.com>
2019-02-04 14:29:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,54 @@
+class Dog
+
+  def name=(name)
+    @name = name
+
+  end
+
+
+
+  def name
+    @name
+  end
+
+
+  def breed=(breed)
+    @breed=breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+
+
+
+
+
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+#
+#   def name=(full_name)
+#     first_name, last_name = full_name.split
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+#
+#   def name
+#     "#{first_name} #{last_name}".strip
+#   end
+#
+#   def breed=(full_name)
+#     first_breed, last_breed = full_breed.split
+#     @first_breed = first_breed
+#     @last_breed = last_breed
+#   end
+#
+#   def breed
+#     "#{first_breed} #{last_breed}".strip
+#   end
+#
+# end
