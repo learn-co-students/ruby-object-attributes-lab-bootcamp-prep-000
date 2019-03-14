fc49964db87ac4f51640a95b84eb49161affde18
Meg Galvez <meg@oakmaine.com>
2019-03-14 16:30:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def name=(their_name)
+    @name = their_name
+  end 
+  def name
+    @name
+  end 
+  
+  def job=(their_job)
+    @job = their_job
+  end 
+  def job
+    @job
+  end 
+end 
