ebe0d8a9b1a9090f2fd8ddb97e3e09543e5bea52
Annia Forney <anniaforney@gmail.com>
2019-04-07 03:18:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed = (breed)
+    @breed = breed
+    end
+  end 
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+ 
+ def initialize(name)
+   @name = name 
+ end 
+ 
+ def name 
+   @name 
+ end 
+ 
+ def job= (job)
+   @job= job
+ end 
+ end 
