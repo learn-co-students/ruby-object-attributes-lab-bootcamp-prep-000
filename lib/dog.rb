8fdfcf3b03479fdb7b889a8a42bcce2c5edd03cb
Danny Lee <dreamer@panix.com>
2019-06-24 21:57:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize()
+    @name = name
+  end
+
+  def name
+    @name
+  end
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def breed
+    @breed
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
+end
