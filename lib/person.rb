e511532d3b85f8c3b075165ecc9df43c122c43d5
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-15 19:12:45 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(set_name)
+    @name = set_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(set_job)
+    @job = set_job
+  end
+
+  def job
+    @job
+  end
+
+end
