7c32930fb3743f9f6c949fd766226cbf838f5f96
Andrew Zajac <ajzajac@icloud.com>
2019-08-01 19:19:23 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def name=(person)
+    @name = person
+  end
+  
+  def name
+    @name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(jobname)
+    @job = jobname
+  end
+end
+
