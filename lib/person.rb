25a80288f5e641dda4bd33a04fc06ae48b0397b1
Mark Kauffman <markkauffman22@gmail.com>
2019-10-17 17:13:17 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+
+class Person
+  def initialize() # Note - no need to pass mandatory args.
+    @name = name
+    @job = job
+  end
+  
+  def name  # Getter
+    @name
+  end
+  
+  def name=(new_name)  #Setter
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+end
+
--- spec/ruby_object_attributes_spec.rb
+++ spec/ruby_object_attributes_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative './spec_helper'
 
 describe 'Dog' do
   it 'is defined within lib/dog.rb' do
