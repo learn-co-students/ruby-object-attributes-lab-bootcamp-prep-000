82d6ab06ebaecd9629d062b12f4f0891bcbbd839
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-29 16:24:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person 
+  
+  # def initialize(name,job)
+  #   @name = name 
+  #   @job = job 
+  # end 
+    
+  def name # get person name 
+     @name
+  end 
+  def name=(name_person) # set person name 
+    @name = name_person
+  end
+  
+  def job # get job 
+    @job
+  end 
+  def job=(job_description) #set job 
+    @job = job_description
+  end
+    
+end 
