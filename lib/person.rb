95ab5a8cd1a97a5491abcdcc0c7545a249721df0
Marcelo Martinez <marcelomst1@gmail.com>
2020-03-07 18:12:40 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name=(name_person)
+    @name = name_person
+  end 
+  def name 
+    @name 
+  end 
+  def job=(name_job)
+    @job = name_job
+  end 
+  def job 
+    @job 
+  end 
+end 
