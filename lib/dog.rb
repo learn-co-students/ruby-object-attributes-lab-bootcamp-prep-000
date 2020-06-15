19fa9160ead676f2d34d92f6c13f731b623fe765
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-06-14 20:23:48 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def name
+        @name
+    end
+    def name=(this_dogs_name)
+        @name=this_dogs_name
+    end
+    def breed
+        @breed
+    end
+    def breed=(this_dogs_breed)
+        @breed=this_dogs_breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def name
+        @name
+    end
+    def name=(this_persons_name)
+        @name=this_persons_name
+    end
+    def job
+        @job
+    end
+    def job=(this_persons_job)
+        @job=this_persons_job
+    end
+end
