fce95fe7837519fa48f22d640236f0fc5c63668a
Mimi C <mina123fire@yahoo.com>
2020-10-03 08:05:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+
+  def breed=(define_breed)
+    @breed = define_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(define_job)
+    @job = define_job
+  end
+  
+  def job
+    @job
+  end
+end
