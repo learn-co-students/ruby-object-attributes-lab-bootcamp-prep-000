cf8a3f5986bdec4a8cc126735e1721a6c58fb5c5
Pascal Byabasaija <basaijapascal9@gmail.com>
2020-12-02 15:33:51 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(name="Beyonce", job="Singer")
+    @name = name
+    @job = job
+  end
+  def name
+    @name
+  end
+  def name=(name)
+    @name
+  end
+  def job
+    @job
+  end
+  def job=(job)
+    @job
+  end
+  
+end
