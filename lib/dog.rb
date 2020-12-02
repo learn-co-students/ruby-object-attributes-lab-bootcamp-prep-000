f662d1f954132e5df21688d7f90043f596191126
Pascal Byabasaija <basaijapascal9@gmail.com>
2020-12-02 15:23:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name="Fido", breed="Beagle")
+    @name = name
+    @breed = breed
+  end
+  def name 
+   @name
+ end
+ def name=(name)
+   @name
+ end
+ def breed
+   @breed
+ end 
+ def breed=(breed)
+   @breed
+ end
+end
