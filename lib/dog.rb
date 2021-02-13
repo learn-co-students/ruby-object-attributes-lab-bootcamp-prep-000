8fe7fbdfbf6c7351f098bfdde9073b0824840215
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2021-02-13 10:10:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+  def name
+    @name
+  end
+  
+  def name= (dog_name)
+    @name = dog_name
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def breed= (breed_title)
+    @breed = breed_title
+  end
+end
+
+class Person
+  def name
+    @name
+  end
+  
+  def name= (name_input)
+    @name = name_input
+  end
+  
+  def job 
+    @job
+  end
+  
+  def job= (job_title)
+    @job = job_title
+  end
+end
